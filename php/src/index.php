5085102a376abc9dcea27f7a2398867f8f4f9139
junmarktimon <doymakey@gmail.com>
2021-12-30 17:39:23 +0800

    fixing form

--- php/src/index.php
+++ php/src/index.php
@@ -33,7 +33,7 @@
       <input type="hidden" name="">
     </div>
     <div class="col-4">
-			<form>
+			<form action="crud.php" method="POST">
 				<div class="mb-3">
 					<label for="exampleInputEmail1" class="form-label">Complete Name: </label>
 					<input type="text" class="form-control" id="name">
